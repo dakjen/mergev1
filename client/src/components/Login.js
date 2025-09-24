6950a3140b0e2160ec7d4fc15ca0de7c4ca1cca4
dakotah jennifer <dakotahjennifer@gmail.com>
2025-09-24 18:12:06 -0400

    refactor: Remove debug log from Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -22,7 +22,7 @@ const Login = ({ onLoginSuccess }) => {
           withCredentials: true
         };
         const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/companies`, config);
-        console.log('API Response:', res.data);
+
         setCompanies(res.data);
         setLoading(false);
       } catch (err) {
