a46d0ef72c3445ad7dca355e597df1a3e29c8e33
dakotah jennifer <dakotahjennifer@gmail.com>
2025-10-01 20:20:00 -0400

    fix: restrict cors to frontend domain

--- server/server.cjs
+++ server/server.cjs
@@ -7,12 +7,12 @@ const prisma = require('./utils/prisma.cjs');
 const app = express();
 
 // Init Middleware
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  next();
-});
+const corsOptions = {
+  origin: 'https://mergev1-78hi.vercel.app',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
